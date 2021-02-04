8dec44375eaee94d603ed9b043c63bd29daf2a70
sayed.fayaz <sdfayaz_008@yahoo.com>
2021-02-04 18:55:11 +0530

    first commit

---
+++ aws.tf
@@ -0,0 +1,11 @@
+provider "aws"{
+    region = "ap-south-1"
+}
+resource "aws_instance" "testterrafrom" {
+      ami = "ami-08e0ca9924195beba"
+      instance_type = "t2.micro"
+ tags = {
+    Name = "Myappllication"
+  }
+
+}
