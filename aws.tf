dabd74a17fc280b1ed245bd5beec28f451904678
sayed.fayaz <sdfayaz_008@yahoo.com>
2021-02-05 20:42:15 +0530

    third commit

--- aws.tf
+++ aws.tf
@@ -1,16 +1,21 @@
 provider "aws"{
     region = "ap-south-1"
 }
+
 resource "aws_instance" "testterrafrom" {
       ami = "ami-08e0ca9924195beba"
       instance_type = "t2.micro"
-provisioner "file" {
-    source      = "**/*.war"
-    destination = "/demo.war"
-  }
+      userdata = <<- EOF
+#!/bin/bash
+sudo su 
+yum update 
+yum install tomcat -y
+systemctl start tomcat 
+EOF
+}
 
  tags = {
     Name = "Myappllication"
-  }
-
 }
+
+
