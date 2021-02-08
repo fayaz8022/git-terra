cea147c6d492b5f57fc19719e34c4370ce857cd7
sayed.fayaz <sdfayaz_008@yahoo.com>
2021-02-08 13:11:05 +0530

    fifth commit

--- aws.tf
+++ aws.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "testterrafrom" {
       ami = "ami-08e0ca9924195beba"
       instance_type = "t2.micro"
-user_data= <<-EOF
+user_data= <<- EOF
 #!/bin/bash
 sudo su 
 yum update 
